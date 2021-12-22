7021b1585a6034b3a7fcb5aac1627ed236d56c1d
Ryota <ryoenoki2013@gmail.com>
2021-12-22 19:40:38 +0900

    created start game function

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ let hasBlackjack = false
 let isAlive = true
 let message = ""
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-  message = "Blackjack!"
-  hasBlackjack = true
-} else {
-  message = "Bust!"
-  isAlive = false
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+  } else if (sum === 21) {
+    message = "Blackjack!"
+    hasBlackjack = true
+  } else {
+    message = "Bust!"
+    isAlive = false
+  }
+  console.log(message);
 }
-
-console.log(message);
