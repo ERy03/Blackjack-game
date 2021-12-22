002ad305a32e3af50f43b841577df10ee44497c1
Ryota <ryoenoki2013@gmail.com>
2021-12-22 18:24:57 +0900

    read me and if else condition added

---
+++ index.js
@@ -0,0 +1,11 @@
+let firstCard = 6
+let secondCard = 9
+let sum = firstCard + secondCard
+
+if (sum < 21) {
+  console.log("Do you want to draw a new card?");
+} else if (sum === 21) {
+  console.log("Blackjack!");
+} else {
+  console.log("Bust!");
+}
