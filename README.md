3af2eeaf18365f31bcb7b96f0f418cc4d1b545f4
Ryota <ryoenoki2013@gmail.com>
2021-12-23 00:53:45 +0900

    query selector

--- README.md
+++ README.md
@@ -7,3 +7,18 @@
 
 100 === "100" // returns false
 ```
+
+### querySelector
+Dynamic method
+we need to be specific for the type of value we are passing inside
+
+unlike `getElementById` we are not specifying that we are looking for an id hence we need to tell it that it's an id using `#sum-el`
+
+query = question
+Selector = CSS selectors
+
+``` javascript
+let sumEl = document.querySelector("#sum-el") // for an id selector
+let sumEl = document.querySelector(".sum-el") // for an class selector
+let sumEl = document.querySelector("body") // for the whole element
+```
--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 let firstCard = 6
-let secondCard = 9
+let secondCard = 10
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
 
 function startGame() {
   if (sum <= 20) {
     message = "Do you want to draw a new card?"
+    sumEl.textContent = "Sum: " + sum
   } else if (sum === 21) {
     message = "Blackjack!"
+    sumEl.textContent = "Sum: " + sum
     hasBlackjack = true
   } else {
     message = "Bust!"
+    sumEl.textContent = "Sum: " + sum
     isAlive = false
   }
   messageEl.textContent = message
