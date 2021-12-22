8f4fd5abe396445acb21348194ae6b3e7fe3d574
Ryota <ryoenoki2013@gmail.com>
2021-12-22 18:47:09 +0900

    created variable hasBlackjack and isAlive

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let firstCard = 6
 let secondCard = 9
 let sum = firstCard + secondCard
+let hasBlackjack = false
+let isAlive = true
 
 if (sum <= 20) {
   console.log("Do you want to draw a new card?");
 } else if (sum === 21) {
   console.log("Blackjack!");
+  hasBlackjack = true
 } else {
   console.log("Bust!");
+  isAlive = false
 }
