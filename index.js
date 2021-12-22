5b035dfa181120a438617d35f85a62cfaea9ccc5
Ryota <ryoenoki2013@gmail.com>
2021-12-23 01:20:32 +0900

    added array

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 6
 let secondCard = 10
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
@@ -13,7 +14,7 @@ function startGame() {
 }
 
 function renderGame() {
-  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
   sumEl.textContent = "Sum: " + sum
   if (sum <= 20) {
     message = "Do you want to draw a new card?"
